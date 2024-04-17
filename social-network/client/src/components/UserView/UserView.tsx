05e0cec0f3aef929bf9b9ffb9e0a851f947214a9
Александр Митрофанов <www.lexter70@gmail.com>
2024-04-17 21:52:26 +0300

    validation data 4.3

--- social-network/client/src/components/UserView/UserView.tsx
+++ social-network/client/src/components/UserView/UserView.tsx
@@ -1,3 +1,5 @@
+import { User } from '../../api/User';
+import { FC } from 'react';
 import {
   getColorIndexByUsername,
   getColorByIndex,
@@ -5,20 +7,24 @@ import {
 } from './getColorByUsername';
 import './UserView.css';
 
-export const UserView = ({ user }) => {
+interface userViewProps {
+  user: User;
+}
+
+export const UserView: FC<userViewProps> = ({ user }) => {
   const colorIndex = getColorIndexByUsername(user.username);
 
   return (
-    <div className="user-view">
+    <div className='user-view'>
       <div
-        className="user-view__avatar"
+        className='user-view__avatar'
         style={{ background: getGradientByIndex(colorIndex) }}
       >
         {user.username.slice(0, 1).toUpperCase()}
       </div>
 
       <span
-        className="user-view__username"
+        className='user-view__username'
         style={{ color: getColorByIndex(colorIndex) }}
       >
         {user.username}
