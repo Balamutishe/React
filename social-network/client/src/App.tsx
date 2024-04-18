563fdebf42427efe3c3caa2d1ac09410ce7cb4af
Александр Митрофанов <www.lexter70@gmail.com>
2024-04-18 20:51:54 +0300

    response data 4.4

--- social-network/client/src/App.tsx
+++ social-network/client/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import { FetchPostListView } from './components/PostListView/FetchPostListView';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className='app'>
+      <FetchPostListView />
+    </div>
+  );
 }
 
 export default App;
