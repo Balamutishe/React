0d91b7436fc1fa6f72c91fd1a2e6deae8ad95e49
Александр Митрофанов <www.lexter70@gmail.com>
2024-09-23 21:33:17 +0300

    Добавлен компонент Logo

--- react-final/src/ui/BaseLayout/BaseLayout.tsx
+++ react-final/src/ui/BaseLayout/BaseLayout.tsx
@@ -1,9 +1,12 @@
+import { Logo } from '../Logo/Logo';
 import './BaseLayout.css';
 
 export const BaseLayout = () => {
   return (
     <>
-      <header className='container'></header>
+      <header className='container'>
+        <Logo />
+      </header>
       <main className='container'></main>
       <footer className='container'></footer>
     </>
---
+++ react-final/src/ui/Logo/Logo.tsx
@@ -0,0 +1,9 @@
+import HeaderLogo from '../../assets/header-logo.svg?react';
+
+export const Logo = () => {
+  return (
+    <div className='header__logo'>
+      <HeaderLogo />
+    </div>
+  );
+};
