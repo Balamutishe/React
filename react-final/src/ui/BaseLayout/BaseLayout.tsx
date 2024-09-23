51255bef57c2fcc5e9a757b150f96fd0b6fe3db3
Александр Митрофанов <www.lexter70@gmail.com>
2024-09-23 21:02:17 +0300

    новый старт финальной работы, начал с создания базовой разметки BaseLayout

---
+++ react-final/src/ui/BaseLayout/BaseLayout.tsx
@@ -0,0 +1,11 @@
+import './BaseLayout.css';
+
+export const BaseLayout = () => {
+  return (
+    <>
+      <header className='container'></header>
+      <main className='container'></main>
+      <footer className='container'></footer>
+    </>
+  );
+};
