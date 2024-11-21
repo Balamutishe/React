ef26a166b7f1de84e3ed33231b1501f2db8174c4
Alexandr Mitrofanov <www.lexter70@gmail.com>
2024-11-21 20:50:46 +0300

    Стилизован дропдаунлист в мобильном разрешении

--- react-final/src/ui/DropdownList/DropdownList.tsx
+++ react-final/src/ui/DropdownList/DropdownList.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
-import { FC } from 'react';
+import { Link } from "react-router-dom";
+import { FC } from "react";
 
-import { TMovieList } from '../../api/Movie';
-import { FilmRaiting } from '../FilmRaiting/FilmRaiting';
+import { TMovieList } from "../../api/Movie";
+import { FilmRaiting } from "../FilmRaiting/FilmRaiting";
 
-import './DropdownList.css';
+import "./DropdownList.css";
 
 interface IDropdownList {
   filteredList: TMovieList;
@@ -18,44 +18,46 @@ export const DropdownList: FC<IDropdownList> = ({
   return (
     <div
       className={
-        searchParamMovie === ''
-          ? 'dropdown dropdown_invisible'
-          : 'dropdown dropdown_visible'
+        searchParamMovie === ""
+          ? "dropdown dropdown_invisible"
+          : "dropdown dropdown_visible"
       }
     >
-      <ul className='dropdown__list'>
+      <ul className="dropdown__list">
         {filteredList.length !== 0
           ? filteredList.map((movie) => (
-              <li key={movie.id} className='dropdown__list-item'>
-                <Link to={`/movie/${movie.id}`} className='dropdown__link'>
-                  <div className='movie-search'>
-                    <div className='movie-search__content_left'>
-                      <img src={movie.posterUrl} alt='#' />
+              <li key={movie.id} className="dropdown__list-item">
+                <Link to={`/movie/${movie.id}`} className="dropdown__link">
+                  <div className="movie-search">
+                    <div className="movie-search__content_left">
+                      <img src={movie.posterUrl} alt="#" />
                     </div>
-                    <div className='movie-search__content_right'>
-                      <div className='movie-search__about'>
+                    <div className="movie-search__content_right">
+                      <div className="movie-search__about">
                         <FilmRaiting
                           raiting={Number(movie.tmdbRating.toFixed(1))}
                         />
-                        <span className='movie-search__relase-year'>
+                        <span className="movie-search__relase-year">
                           {movie.releaseYear}
                         </span>
                         <span>
                           {movie.genres.map((genre, index) => (
-                            <span className='movie-search__genre' key={index}>
+                            <span className="movie-search__genre" key={index}>
                               {genre}
                             </span>
                           ))}
                         </span>
-                        <span>{movie.runtime} мин</span>
+                        <span className="movie-runtime">
+                          {movie.runtime} мин
+                        </span>
                       </div>
-                      <h2 className='movie-search__title'>{movie.title}</h2>
+                      <h2 className="movie-search__title">{movie.title}</h2>
                     </div>
                   </div>
                 </Link>
               </li>
             ))
-          : 'Фильм не найден'}
+          : "Фильм не найден"}
       </ul>
     </div>
   );
