44f74d11fe1379e9a018451a8dad296ccc59d311
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-02-24 17:16:00 +0300

    create template

---
+++ gorestApp/src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+export const App = () => {
+  return (
+    <>
+      <header className="header">
+        <h1>TodoList</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default App;
