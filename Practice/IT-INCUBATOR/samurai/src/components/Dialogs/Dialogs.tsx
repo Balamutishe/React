e61ee55bfd2a468a587f9e7e21c339e4c52ccb85
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-03-24 20:11:41 +0300

    refactor style

--- Practice/IT-INCUBATOR/samurai/src/components/Dialogs/Dialogs.tsx
+++ Practice/IT-INCUBATOR/samurai/src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,30 @@ import c from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-            <div className={ c.dialogs }>
-                Dialogs
+            <div className={ c.container }>
+                <div className={ c.dialogs }>
+                    <h2 className={ c.title }>Dialogs</h2>
+                    <ul className={ c.list }>
+                        <li className={ c.item }>UserDialog</li>
+                        <li className={ c.item }>UserDialog</li>
+                        <li className={ c.item }>UserDialog</li>
+                        <li className={ c.item }>UserDialog</li>
+                        <li className={ c.item }>UserDialog</li>
+                        <li className={ c.item }>UserDialog</li>
+                    </ul>
+                </div>
+                <div className={ c.messages }>
+                    <h2 className={ c.title }>Messages</h2>
+                    <ul className={ c.list }>
+                        <li className={ c.item }>UserMessage</li>
+                        <li className={ c.item }>UserMessage</li>
+                        <li className={ c.item }>UserMessage</li>
+                        <li className={ c.item }>UserMessage</li>
+                        <li className={ c.item }>UserMessage</li>
+                        <li className={ c.item }>UserMessage</li>
+                    </ul>
+                </div>
             </div>
+
     )
 }
