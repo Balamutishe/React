fa88f821e3ff3426c639cf9c57f3a19e97f5791c
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-06-26 13:23:59 +0300

    add server folder

--- Practice/TODOLIST/client/src/widgets/Navigation/NavigationList.tsx
+++ Practice/TODOLIST/client/src/widgets/Navigation/NavigationList.tsx
@@ -7,20 +7,28 @@ import { handlerSwitchIcon } from "@shared/utils/handlerSwitchIcon";
 export const NavigationList = () => {
   const [isTasksListVisible, setIsTasksListVisible] = useState(false);
 
-  const handlerTasksListVisibility = () => {
-    setIsTasksListVisible((prev) => !prev);
+  const handlerTasksListVisibilityClick = () => {
+    setIsTasksListVisible(true);
+  };
+
+  const handlerTasksListVisibilityBlur = () => {
+    setIsTasksListVisible(false);
   };
 
   return (
     <ul className={c.list}>
-      <li key={crypto.randomUUID()} className={c.listItem}>
+      <li
+        key={crypto.randomUUID()}
+        className={c.listItem}
+        onClick={handlerTasksListVisibilityBlur}
+      >
         {handlerSwitchIcon("Dashboard", c.navIcon)}
         <NavigationLink to={"/dashboard"} textLink={"Dashboard"} />
       </li>
       <li
         key={crypto.randomUUID()}
         className={c.listItem}
-        onClick={handlerTasksListVisibility}
+        onClick={handlerTasksListVisibilityClick}
       >
         <div>
           <div className={c.navTasks}>
@@ -34,7 +42,11 @@ export const NavigationList = () => {
           <NavigationTasksList visible={isTasksListVisible} />
         </div>
       </li>
-      <li key={crypto.randomUUID()} className={c.listItem}>
+      <li
+        key={crypto.randomUUID()}
+        className={c.listItem}
+        onClick={handlerTasksListVisibilityBlur}
+      >
         {handlerSwitchIcon("Calendar", c.navIcon)}
         <NavigationLink to={"/calendar"} textLink={"Calendar"} />
       </li>
