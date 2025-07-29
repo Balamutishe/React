dcb1cc62a4f3b51d895a31d875417dd14a32c820
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-07-29 20:41:53 +0300

    основа для создания приложений с использованием tanstack

---
+++ Practice/TANSTACK-TEMPLATE/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/app/routes",
+      generatedRouteTree: "./src/app/routes/routeTree.gen.ts",
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
+  server: {
+    watch: {
+      // Исключаем db.json из наблюдения
+      ignored: ["**/db.json"],
+    },
+  },
+});
