53e85486cf75fa271c87cca463df2b070b74ec80
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-07-29 20:44:17 +0300

    основа приложения

---
+++ Practice/CLIENTSLIST/src/app/main.tsx
@@ -0,0 +1,25 @@
+import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { queryClient } from "@shared/api";
+
+import { routeTree } from "./routes/routeTree.gen.ts";
+
+import "./styles/index.css";
+import "./styles/reset.css";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
