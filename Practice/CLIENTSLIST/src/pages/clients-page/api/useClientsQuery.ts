9927653ccd70fb74e19f4e471d20e1516a3ce071
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-08-06 17:14:34 +0300

    изменена версия json-server на более позднюю

--- Practice/CLIENTSLIST/src/pages/clients-page/api/useClientsQuery.ts
+++ Practice/CLIENTSLIST/src/pages/clients-page/api/useClientsQuery.ts
@@ -1,32 +1,18 @@
-import type { TClient } from "@shared/types";
 import { useQuery } from "@tanstack/react-query";
 
 export const useClientsQuery = (filter?: string) => {
+  const query = `?name_like=${filter}`;
+
   return useQuery({
-    queryKey: ["clients", "all", filter],
+    queryKey: ["clients", "all", query],
     queryFn: async () => {
-      return await fetch(`http://localhost:3001/clients`).then(async (res) => {
-        const data = await res.json();
+      return await fetch(`http://localhost:3001/clients${query}`).then(
+        async (res) => {
+          const data = await res.json();
 
-        if (filter) {
-          const filterData = data.filter((item: TClient) => {
-            console.log(Object.values(item).join());
-            if (
-              Object.values(item)
-                .join()
-                .toLowerCase()
-                .trim()
-                .includes(filter.toLowerCase())
-            ) {
-              return item;
-            }
-          });
-
-          return filterData;
+          return data;
         }
-
-        return data;
-      });
+      );
     },
   });
 };
