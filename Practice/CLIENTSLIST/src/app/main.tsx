e479c028a8eb9830ab820ef8c0c1d911ed2b53dc
Alexander Mitrofanov <www.lexter70@gmail.com>
2025-08-10 16:30:12 +0300

    template next

--- Practice/CLIENTSLIST/src/app/main.tsx
+++ Practice/CLIENTSLIST/src/app/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
@@ -8,7 +9,6 @@ import { routeTree } from "./routes/routeTree.gen.ts";
 
 import "./styles/index.css";
 import "./styles/reset.css";
-import { StrictMode } from "react";
 
 const router = createRouter({ routeTree });
 
