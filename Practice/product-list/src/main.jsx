c123501897396fad89b2c043130d32a4b76794eb
Balamutishe <126945241+Balamutishe@users.noreply.github.com>
2025-09-05 15:40:00 +0300

    Delete Practice/product-list directory

--- Practice/product-list/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { ProductsList } from "./ProductsList";
-import { products } from "./products";
-
-const productsList = document.getElementById("products-list");
-
-const reactRoot = createRoot(productsList);
-
-reactRoot.render(<ProductsList products={products} />);
